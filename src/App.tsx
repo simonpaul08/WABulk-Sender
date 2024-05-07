04283b68f633d8cdd2f52f6f73c0cb4cf2f233c7
Simon Paul <samonpaul06@gmail.com>
2024-05-07 23:20:13 +0530

    upgraded to ts

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [isTab, setIsTab] = useState(false);
+
+
+  useEffect(() => {
+    const checkTab = async () => {
+      const [tab] = await chrome.tabs.query({ active: true });
+      if (tab.url?.startsWith("https://web.whatsapp.com")) {
+        // show form  
+        setIsTab(true)
+        console.log(isTab)
+      } else {
+        // show open whatsapp button
+        setIsTab(false)
+      }
+
+      checkTab()
+    }
+  }, [])
+
+  return (
+    <div className='main'>
+      <div className="container">
+        <h3 className='container-title'>WABulk Sender</h3>
+
+        {
+        isTab ?
+          <div className='upload-block'>
+            <p className='upload-text'>Upload an excel file</p>
+            <input type="file" className='upload-input' />
+            <button className='upload-btn' type='button'>Upload</button>
+          </div>
+          :
+          <div className='upload-block'>
+            <button className='upload-btn' type='button'>Open WhatsApp</button>
+          </div>
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default App
